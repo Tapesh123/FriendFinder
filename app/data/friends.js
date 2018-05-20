a471651a23d6c7283b1414d834a993906f75c34b
Patkul <tshah011@gmail.com>
2018-05-20 10:52:20 -0400

    1

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [{
+    name: "Margot Robbie",
+    photo: "https://media.giphy.com/media/Q9B24UDmvoD96/giphy.gif",
+    scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},{
+    name: 'Deepika Padukone',
+    photo: 'https://media.giphy.com/media/3ohfFx7uDpkl6qP0D6/giphy.gif',
+    scores: [
+        2,
+        4,
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '2',
+        '1',
+        '5'
+    ]
+},
+ { 
+    name: "Emma Watson",
+    photo: "https://media.giphy.com/media/N0AxCgHxffkvm/giphy.gif",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+}, {
+    name:"Goofy",
+    photo:"https://media.giphy.com/media/2wKbtCMHTVoOY/giphy.gif",
+    scores:[
+        3,
+        1,
+        2,
+        4,
+        1,
+        1,
+        2,
+        3,
+        4,
+        5
+      ]
+  },{
+    name:"Matt Damon",
+    photo:"https://media.giphy.com/media/h1JWMpBEH9PBm/giphy.gif",
+    scores:[
+        3,
+        1,
+        2,
+        4,
+        1,
+        1,
+        2,
+        3,
+        4,
+        5
+      ]
+
+
+
+
+
+  }]
+  // Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
